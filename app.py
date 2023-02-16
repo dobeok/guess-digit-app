0008571b01ae2358867e77699b735324cd4b2243
do92 <voduydo@gmail.com>
2023-02-16 13:48:31 +0700

    changed to plotly

--- app.py
+++ app.py
@@ -5,10 +5,30 @@ from streamlit_drawable_canvas import st_canvas
 from io import BytesIO
 from skimage.transform import resize
 import numpy as np
+import plotly.express as px
 
 
-# model = tf.keras.models.load_model('./predict-model/results/keras_mnist_linear_stack.h5')
 model = tf.keras.models.load_model('./predict-model/results/keras_mnist_cnn.h5')
+
+st.set_page_config(layout="wide")
+
+
+def predict(image, model):
+    # bw only
+    image_bw = image[:, :, 3]
+    
+    # if the model is cnn
+    image_bw_28 = resize(image_bw, (28, 28))
+    
+    # if the model is linear
+    # downsize image to 28 x 28
+    # image_bw_28 = image_bw_28.reshape(784, -1)
+
+    predicted_probs = model.predict(np.array([image_bw_28]))
+    predicted_label = np.argmax(predicted_probs, axis=1)
+
+    return predicted_probs, predicted_label
+
 
 with st.sidebar:
     drawing_mode = st.radio(
@@ -26,62 +46,81 @@ with st.sidebar:
         )
 
 
-col_1, col_2 = st.columns(2)
-
+col_1, col_2 = st.columns([2, 1], gap='medium')
 
 with col_1:
-    st.title('Draw in this box')
+    st.markdown('### Draw in this box')
     canvas_result = st_canvas(
-        fill_color="rgba(255, 165, 0, 0.3)",
+        fill_color="rgba(255, 255, 255, 0)",
         stroke_width=14,
         update_streamlit=True,
-        height=256,
-        width=256,
+        height=225,
+        width=225,
         drawing_mode=drawing_mode,
         key="canvas",
     )
 
-    
+ 
+    img_data = canvas_result.image_data
 
 
-def predict(image, model):
-    # bw only
-    image_bw = image[:, :, 3]
 
-    # downsize image to 28 x 28
-    image_bw_28 = resize(image_bw, (28, 28))
-    # image_bw_28 = image_bw_28.reshape(784, -1)
+    if img_data is not None:
+        probabilities, guess = predict(img_data, model)
 
-    predicted_probs = model.predict(np.array([image_bw_28]))
-    predicted_label = np.argmax(predicted_probs, axis=1)
-
-    return predicted_probs, predicted_label
-
-
-img_data = canvas_result.image_data
-if img_data is not None:
-    with col_2:
-        if not np.all(img_data == img_data[0]):
-            probabilities, guess = predict(img_data, model)
-            
-            if probabilities[0].max() >= THRESHOLD:
-                st.title(f'Predicted = {guess[0]}')    
-            else:
-                st.title(f"I'm not very sure..")
-
-            st.bar_chart(
-                probabilities[0]
+        fig = px.bar(
+            x=range(10),
+            y=[0.01 for _ in range(10)]
             )
 
-    # download button
-    result = Image.fromarray(img_data)
-    buf = BytesIO()
-    result.save(buf, format='PNG')
-    byte_im = buf.getvalue()
+        sorted_probabilities = sorted(probabilities[0], reverse=True)
+        
+        if not np.all(img_data == img_data[0]):
+            if sorted_probabilities[0] >= THRESHOLD:
+                st.markdown(f'### Prediction = {guess[0]}')
+            else:
+                st.markdown(f'### Please draw again..')
 
-    st.download_button(
-        label='download img',
-        data=byte_im,
-        file_name="downloaded_image.png",
-        mime="image/png",
-        )
+        st.markdown('### Probabilities')
+        if not np.all(img_data == img_data[0]):
+            if probabilities[0].max() >= THRESHOLD:
+                fig = px.bar(x=range(10), y=probabilities[0])
+                
+        
+        fig.update_layout(
+            yaxis={
+                'title': None,
+                # 'visible': False,
+                # 'showticklabels': False,
+                'range': [0, 1]
+                },
+            xaxis={
+                'title':None,
+                'dtick': 1,
+            }
+            )
+
+
+        st.plotly_chart(
+            fig,
+            theme='streamlit',
+            use_container_width=True,
+            config={
+                'displayModeBar': False,
+                }
+            )
+
+
+with st.sidebar:
+    if img_data is not None:
+        result = Image.fromarray(img_data)
+        buf = BytesIO()
+        result.save(buf, format='PNG')
+        byte_im = buf.getvalue()
+
+        st.download_button(
+            label='download image',
+            data=byte_im,
+            file_name="downloaded_image.png",
+            mime="image/png",
+            )
